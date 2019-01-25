c06067d75dd673aa69d0ea3fe11d9cb9d96883b1
probobbo <probobbo@gmail.com>
2019-01-25 22:21:03 +0100

    WIP: smarmottamento

---
+++ Assets/Scripts/QuickTimeEvent/QuickTimeBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTimeBar : MonoBehaviour
+{
+
+	private List<QuickTimeButton> _buttons;
+	public GameObject ButtonPrefab;
+
+	private void Start()
+	{
+		_buttons = new List<QuickTimeButton>();
+	}
+
+	public void AddButton()
+	{
+		GameObject g = Instantiate(ButtonPrefab, transform);
+		_buttons.Add(g.GetComponent<QuickTimeButton>());
+	}
+
+	//todo
+	public void CheckButton() { }
+
+	//
+}
---
+++ Assets/Scripts/QuickTimeEvent/QuickTimeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class QuickTimeButton : MonoBehaviour
+{
+	private Image _buttonImage;
+	private QuickTimeBar _quickTimeBar;
+	private Vector3 _targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		_buttonImage = GetComponent<Image>();
+		_quickTimeBar = GetComponentInParent<QuickTimeBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
