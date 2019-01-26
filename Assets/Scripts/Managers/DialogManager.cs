8c2166159e9331b77265159fa4c79e5a17bb1a5b
gench23 <gench23@gmail.com>
2019-01-26 04:29:08 +0100

    scripting dialog manager

--- Assets/Scripts/Managers/DialogManager.cs
+++ Assets/Scripts/Managers/DialogManager.cs
@@ -2,43 +2,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
 	[Serializable]
 	public class Dialogs
 	{
+		[Serializable]
+		public class Dialog
+		{
+			public int id;
+			public DialogTexts dialog;
+		}
+
+		[Serializable]
+		public class DialogTexts
+		{
+			public string text;
+			public Answer[] answers;
+		}
+
+		[Serializable]
+		public class Answer
+		{
+			public string text;
+			public int idtojumpto;
+		}
+
 		public Dialog[] dialogs;
 	}
 
-	[Serializable]
-	public class Dialog
-	{
-		public int id;
-		public DialogTexts dialog;
-	}
-
-	[Serializable]
-	public class DialogTexts
-	{
-		public string text;
-		public Answer[] answers;
-	}
-
-	[Serializable]
-	public class Answer
-	{
-		public string text;
-		public int idtojumpto;
-	}
-
+	public string NPC;
 	public Dialogs _dialogs;
 
-	// Start is called before the first frame update
+	[SerializeField] private Image _dialogPanel;
+	[SerializeField] private TextMeshProUGUI _dialogText;
+	[SerializeField] private TextMeshProUGUI[] _answers;
+
+	private int _dialogStepIndex = 0;
+
 	private void Start()
 	{
-		_dialogs = ReadAndParseJson<Dialogs>(Path.Combine(Application.streamingAssetsPath, "vecchia.json"));
+		_dialogs = ReadAndParseJson<Dialogs>(Path.Combine(Application.streamingAssetsPath, NPC + ".json"));
+
+		_dialogPanel.gameObject.SetActive(false);
+
+		EventManager.Instance.OnPlayingStateChanged.AddListener(StartDialog);
 	}
 
 	private T ReadAndParseJson<T>(string path)
@@ -47,9 +59,68 @@ public class DialogManager : MonoBehaviour
 		return JsonUtility.FromJson<T>(jsonText);
 	}
 
-	// Update is called once per frame
-	private void Update()
+	private void StartDialog(GameManager.PlayingState state)
 	{
+		if (state == GameManager.PlayingState.Dialoguing)
+		{
+			_dialogPanel.gameObject.SetActive(true);
+			NextDialogStep();
+		}
+	}
 
+	public void NextDialogStep()
+	{
+		if (_dialogStepIndex != -1)
+		{
+			_dialogText.text = _dialogs.dialogs[_dialogStepIndex].dialog.text;
+			var answers = _dialogs.dialogs[_dialogStepIndex].dialog.answers;
+			for (int i = 0; i < _answers.Length; i++)
+			{
+				if (i < answers.Length)
+					_answers[i].text = answers[i].text;
+				else
+					_answers[i].text = "";
+			}
+
+			EventManager.Instance.OnButtonPressed.AddListener(CheckAnswer);
+		}
+		else
+		{
+			EventManager.Instance.OnButtonPressed.AddListener(EndDialog);
+		}
+	}
+
+	private void EndDialog(InputManager.ControllerButtons arg)
+	{
+		_dialogPanel.gameObject.SetActive(false);
+
+		EventManager.Instance.OnButtonPressed.RemoveListener(EndDialog);
+		EventManager.Instance.OnPlayingStateChanged.Invoke(GameManager.PlayingState.Running);
+	}
+
+	private void CheckAnswer(InputManager.ControllerButtons buttonPressed)
+	{
+		var answers = _dialogs.dialogs[_dialogStepIndex].dialog.answers;
+		switch (buttonPressed)
+		{
+			case InputManager.ControllerButtons.A:
+				_dialogStepIndex = answers[0].idtojumpto;
+				break;
+			case InputManager.ControllerButtons.B:
+				if (answers.Length > 1)
+					_dialogStepIndex = answers[1].idtojumpto;
+				break;
+			case InputManager.ControllerButtons.X:
+				if (answers.Length > 2)
+					_dialogStepIndex = answers[2].idtojumpto;
+				break;
+			case InputManager.ControllerButtons.Y:
+				if (answers.Length > 3)
+					_dialogStepIndex = answers[3].idtojumpto;
+				break;
+		}
+
+		EventManager.Instance.OnButtonPressed.RemoveListener(CheckAnswer);
+		NextDialogStep();
 	}
 }
---
+++ Assets/Scripts/NPCs/AbstractNPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractNPC : MonoBehaviour
+{
+	private bool wasHit = false;
+
+	private void Update()
+	{
+		Move();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("ote");
+		if (other.CompareTag("Player") && !wasHit)
+		{
+			Debug.Log("hit player");
+			EventManager.Instance.OnPlayingStateChanged.Invoke(GameManager.PlayingState.Dialoguing);
+			wasHit = true;
+		}
+	}
+
+	protected abstract void Move();
+}
