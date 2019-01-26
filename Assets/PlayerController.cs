ce32dc205c8466f1844cc4fde46194a581e1bd04
probobbo <probobbo@gmail.com>
2019-01-26 01:13:00 +0100

    Aggiunti controlli da joystick per il movimento

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float MovementSpeed = 5.0f;
+	private float _e = .05f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector3(0,0,transform.position.z+.2f);
-        }
-        
-        
-    }
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (GameManager.Instance.CurrentState == GameManager.PlayingState.Running)
+		{
+			float forwardWeight = Input.GetAxis("Vertical");
+			float turningWeight = Input.GetAxis("Horizontal");
+			var direction = new Vector3(turningWeight, 0f, forwardWeight);
+			transform.Translate(direction * MovementSpeed * Time.deltaTime, Space.World);
+			if (Mathf.Abs(forwardWeight) >= _e || Mathf.Abs(turningWeight) >= _e)
+				transform.forward = direction;
+		}
+		else
+		{
+			if (Input.GetButtonDown("A"))
+			{
+				EventManager.Instance.OnButtonPressed.Invoke(InputManager.ControllerButtons.A);
+			}
+			if (Input.GetButtonDown("B"))
+			{
+				EventManager.Instance.OnButtonPressed.Invoke(InputManager.ControllerButtons.B);
+			}
+			if (Input.GetButtonDown("X"))
+			{
+				EventManager.Instance.OnButtonPressed.Invoke(InputManager.ControllerButtons.X);
+			}
+			if (Input.GetButtonDown("Y"))
+			{
+				EventManager.Instance.OnButtonPressed.Invoke(InputManager.ControllerButtons.Y);
+			}
+		}
+	}
 }
