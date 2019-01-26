ae0378c2359ae4c0b3c6f9140a5db996c8686a67
probobbo <probobbo@gmail.com>
2019-01-26 12:00:50 +0100

    Aggiunta possibilità di perdere e casa finale (manca interazione)

---
+++ Assets/Scripts/EndPanelController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndPanelController : MonoBehaviour
+{
+	[SerializeField]
+	private Button _retry;
+	[SerializeField]
+	private Button _back;
+	[SerializeField]
+	private GameObject _endPanel;
+	// Start is called before the first frame update
+	void Start()
+	{
+		_retry.onClick.AddListener(() =>
+		{
+			SceneManager.LoadScene(1);
+		});
+
+		_back.onClick.AddListener(()=>
+		{
+			SceneManager.LoadScene(0);
+		});
+		_endPanel.SetActive(false);
+		EventManager.Instance.OnPlayingStateChanged.AddListener(PopUp);
+	}
+
+	private void PopUp(GameManager.PlayingState state)
+	{
+		if (state == GameManager.PlayingState.End)
+		{
+			_endPanel.SetActive(true);
+		}
+	}
+}
